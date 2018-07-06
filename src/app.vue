73cc525e2e6e07e2d200bab51a3298326ce0f6c2
liutaochange <liutaochange@163.com>
2018-07-06 18:53:49 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text">{{text}}</div>
+</template>
+<script>
+  export default{
+    data () {
+      return {
+        text: 'vue project'
+      }
+    }
+  }
+</script>
+<style scoped>
+  .text{
+    font-size: 16px;
+    color: brown
+  }
+</style>
+
