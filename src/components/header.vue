ba38a79ef36238bccf97305d71d9ed958b8dafb5
liutaochange <liutaochange@163.com>
2018-07-11 07:42:35 +0800

    style init

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+   
+  </div>
+</template>
+<script>
+  export default{
+    data () {
+      return {
+        text: 'vue project'
+      }
+    }
+  }
+</script>
+<style scoped>
+  .header{
+    font-size: 16px;
+    color: brown;
+  }
+</style>
+
