f3d165ddbe7762061766873037b2f5e58247fdd8
liutaochange <liutaochange@163.com>
2018-07-16 22:03:01 +0800

    change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'standard',
+  extends: 'eslint:recommended',
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module' //类型为module，因为代码使用了使用了ECMAScript模块
